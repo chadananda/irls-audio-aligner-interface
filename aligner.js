82b3deba9681d1acd3a79884d374ffef5499b6d7
Daniel Jones <djones1844@gmail.com>
2017-02-01 18:18:19 -0800

    update client

--- aligner.js
+++ aligner.js
@@ -4,11 +4,10 @@ var fs = require('fs');
 var cheerio = require('cheerio');
 var request = require('request');
 
-
-
 var aligner = function(){ 
 	var userKey, 
 			server,
+			apiVersion,
 			scripts={	"raw": "./scripts/raw.js",
 								"mp": "./scripts/mp.js",
 								"isBahai": "./scripts/isBahai.js"
@@ -16,7 +15,35 @@ var aligner = function(){
 			job={};
 };
 
-aligner.prototype.load = function (j) {};
+/* create an alignment task with client
+var job = {	// required
+							job_title: "My Book of Foo-Bar",
+							// required - path/url to text file
+							text: "https://dl.dropboxusercontent.com/u/183568/gdm/gdm.html",
+							// required - path/url to audio file
+							audio: "https://dl.dropboxusercontent.com/u/183568/gdm/gdm.ogg", 
+							// UUID for job, if blank one will be generated
+							job_id: "1b99dbdb-c4f8-412c-8f11-87be7adaa5b8",
+							// if blank defaults to "en"
+							language: "en-gb",							
+							// The class or id containing aligned text. Blank defaults to "body"
+							parse_id: ".ocean" 
+						}
+*/  
+aligner.prototype.load = function (j) {
+	if(j.job_title && j.text && j.audio){
+		// set defaults
+		if(!j.job_id){ j.job_id = UUIDv4;}
+	 	if(!j.language){j.language = "en";}
+	 	if(!j.parse_id){j.parse_id = "body";}
+	 	// update the aligner 
+	 	this.job = j;
+		return this.job.job_id;
+	} else {
+		return 0;
+	}
+	var UUIDv4 = function b(a){return a?(a^Math.random()*16>>a/4).toString(16):([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,b)}
+};
 
 // provides previously loaded text
 aligner.prototype.getText = function () {};
@@ -40,5 +67,7 @@ aligner.prototype.addScript = function (s) {};
 // runs scripts on audio file
 //aligner.prototype.normalize = function () {};
 
+aligner = new aligner;
 
-exports.aligner = new aligner;
+
+
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // export aligner code
 exports = module.exports = require('./aligner.js');
+
+
 // export version
 exports.version = require('./package.json').version;
